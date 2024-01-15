bce0cd4956783214ba07872e785927895cb33b52
Lyle Liu <cgglyle@outlook.com>
2024-01-15 13:45:36 +0800

    feat: enable jpa auditing

--- src/main/kotlin/top/cgglyle/boson/security/common/entity/basic/AbstractAuditingEntity.kt
+++ src/main/kotlin/top/cgglyle/boson/security/common/entity/basic/AbstractAuditingEntity.kt
@@ -2,21 +2,26 @@ package top.cgglyle.boson.security.common.entity.basic
 
 import jakarta.persistence.AttributeOverride
 import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.MappedSuperclass
 import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import top.cgglyle.boson.security.user.domain.entity.UID
 import java.time.Instant
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
 abstract class AbstractAuditingEntity(
     @CreatedBy
-    @AttributeOverride(name = "value", column = Column(name = "created_by", updatable = false, nullable = false))
-    val createdBy: UID = UID.defaultUID(),
+    @AttributeOverride(
+        name = "value",
+        column = Column(name = "created_by", updatable = false, nullable = false)
+    )
+    var createdBy: UID = UID.defaultUID(),
 
     @CreatedDate
     @Column(name = "created_date", updatable = false, nullable = false)
-    val createdDate: Instant? = Instant.now()
+    var createdDate: Instant? = Instant.now()
 ) : AbstractIDEntity() {
-
 }
